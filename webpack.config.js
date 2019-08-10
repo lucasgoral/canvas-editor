313ca8f85193287b08f5e5126aa23e0fa4d85b16
Łukasz Goral <lukasz.goral.design@gmail.com>
2019-08-10 12:50:07 +0200

    init project

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new LodashModuleReplacementPlugin
+  ]
+}
+
+module.exports = config;
